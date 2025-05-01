b3e2ce8df3e77f7e66a2319d08b57e391b145a88
Aleksandr Kaidalov <alkaid@taltech.ee>
2025-05-01 16:18:05 +0300

    update: update vite config 12

--- TIC-TAC-TWO-VUE/vite.config.js
+++ TIC-TAC-TWO-VUE/vite.config.js
@@ -5,9 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-      ? '/icd0006-24-25-s/TIC-TAC-TWO-VUE/dist/'
-      : '/',
+  base: '/icd0006-24-25-s/TIC-TAC-TWO-VUE/dist/',
   plugins: [vue()],
   resolve: {
     alias: {

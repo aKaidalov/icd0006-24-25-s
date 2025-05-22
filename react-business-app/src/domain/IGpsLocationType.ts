50cc1e76c3cecc5d5e462aae2aad6c4fea3827c2
Aleksandr Kaidalov <alkaid@taltech.ee>
2025-05-23 01:11:56 +0300

    refactor: delete unnecessary files

--- react-business-app/src/domain/IGpsLocationType.ts
+++
@@ -1,6 +0,0 @@
-import type {IDomainId} from "./IDomainId.ts";
-
-export interface IGpsLocationType extends IDomainId{
-    name: string;
-    description: string;
-}
--- react-business-app/src/domain/IGpsSessionType.ts
+++
@@ -1,8 +0,0 @@
-import type {IDomainId} from "./IDomainId.ts";
-
-export interface IGpsSessionType extends IDomainId{
-    name: string;
-    description: string;
-    paceMin: number;
-    paceMax: number;
-}
